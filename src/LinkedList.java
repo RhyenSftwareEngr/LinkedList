dad8175a37a53c21168ebb26a2e97dfb7b05be0e
RhyenSftwareEngr <jannatividad1313@gmail.com>
2022-08-31 11:10:36 +0800

    linked list added

---
+++ src/LinkedList.java
@@ -0,0 +1,72 @@
+//LINKED LISTS: JAVA
+//        Implementation
+//        You finished your LinkedList class! Now we’re going to create an instance of that class
+//and create a linked list of the seasons. We will add to it, remove from it, and finally print it out to
+//        see what we’ve done.
+
+
+public class LinkedList {
+
+    public static void main(String []args) {
+        // Write your code here:
+        LinkedList seasons = new LinkedList();
+        seasons.printList();
+        seasons.addToHead("summer");
+        seasons.addToHead("spring");
+        seasons.printList();
+        seasons.addToTail("fall");
+        seasons.addToTail("winter");
+        seasons.printList();
+        seasons.removeHead();
+        seasons.printList();
+    }
+
+    public Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void addToHead(String data) {
+        Node newHead = new Node(data);
+        Node currentHead = this.head;
+        this.head = newHead;
+        if (currentHead != null) {
+            this.head.setNextNode(currentHead);
+        }
+    }
+
+    public void addToTail(String data) {
+        Node tail = this.head;
+        if (tail == null) {
+            this.head = new Node(data);
+        } else {
+            while (tail.getNextNode() != null) {
+                tail = tail.getNextNode();
+            }
+            tail.setNextNode(new Node(data));
+        }
+    }
+
+    public String removeHead() {
+        Node removedHead = this.head;
+        if (removedHead == null) {
+            return null;
+        }
+        this.head = removedHead.getNextNode();
+        return removedHead.data;
+    }
+
+    public String printList() {
+        String output = "<head> ";
+        Node currentNode = this.head;
+        while (currentNode != null) {
+            output += currentNode.data + " ";
+            currentNode = currentNode.getNextNode();
+        }
+        output += "<tail>";
+        System.out.println(output);
+        return output;
+    }
+
+}
